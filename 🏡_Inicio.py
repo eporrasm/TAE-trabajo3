b748f03d67a5d120df48bb097f6b0214e4c027e0
EsGarciaC <esgarciac@unal.edu.co>
2022-11-13 14:38:31 -0500

    Update 🏡_Inicio.py

---
+++ 🏡_Inicio.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(page_title="miDataCreditoGratis.com", page_icon="./Graficas/stockfish.png", layout="centered", initial_sidebar_state="auto")
+
+
+
+
+st.title("miDataCreditoGratis.com")
+
+st.markdown("¡Infórmate sobre la página! https://www.youtube.com/watch?v=0QM6W8hIw54&t=11s")
+
+st.markdown("""¡Bienvenido! Esta aplicación le ayudará a averiguar su puntaje 
+            crediticio de manera totalmenta gratuita. Conocer su puntaje crediticio es útil para
+            aquellos que estén pensando en adquirir un préstamo, 
+            una tarjeta de crédito o simplemente estén interesados en la confianza que generan
+            en las entidades financieras. Para comenzar, simplemente 
+            rellene el cuestionario de la izquierda con su información financiera básica. En la parte de abajo podrá visualizar su puntaje crediticio, lo que implica y 
+            cómo éste se compara con el de la persona promedio.""")
