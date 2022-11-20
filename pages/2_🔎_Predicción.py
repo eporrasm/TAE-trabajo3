60b1cc4f6fd389e26696b994a062b09f9097ca91
EsGarciaC <esgarciac@unal.edu.co>
2022-11-20 10:36:09 -0500

    Update 2_🔎_Predicción.py

--- pages/2_🔎_Predicción.py
+++ pages/2_🔎_Predicción.py
@@ -22,39 +22,39 @@ with st.sidebar:
  #es una fecha en el rango que dio el usuario. Después hay que aplicar procesar_fechas a
 #este dataframe y ya queda con el formato para aplicar el modelo.
 
-# try:
-df_fechas = pd.DataFrame(columns=["FECHA"])
-f1,f2 = fechas
-finicial= f1
-while f1 <= f2:
-    df_fechas = df_fechas.append(pd.DataFrame({"FECHA": [f1]}))
-    f1 += datetime.timedelta(days=1)   
-df_fechas=df_fechas.reset_index(drop=True) 
+try:
+    df_fechas = pd.DataFrame(columns=["FECHA"])
+    f1,f2 = fechas
+    finicial= f1
+    while f1 <= f2:
+        df_fechas = df_fechas.append(pd.DataFrame({"FECHA": [f1]}))
+        f1 += datetime.timedelta(days=1)   
+    df_fechas=df_fechas.reset_index(drop=True) 
 
-df_fechas = procesar_fechas(df_fechas)
-#st.write(df_fechas)
-if tipo == "Atropello":
-    predicciones = atropello.predict(df_fechas)
-elif tipo == "Caída de Ocupante":
-    predicciones = caida.predict(df_fechas)
-elif tipo == "Choque":
-    predicciones = choque.predict(df_fechas)
-elif tipo == "Incendio":
-    predicciones = list()
-    for _ in range(df_fechas.shape[0]):
-        r = random.random()
-        if r<=0.012438916037316748:
-            predicciones.append(1)
-        else:
-            predicciones.append(0)
-    predicciones = np.asarray(predicciones)
-elif tipo == "Volcamiento":
-    predicciones = volcamiento.predict(df_fechas)
-elif tipo == "Otro":
-    predicciones = otro.predict(df_fechas)
-    predicciones = np.asarray(tuple(map(piso_techo, predicciones)))
+    df_fechas = procesar_fechas(df_fechas)
+    #st.write(df_fechas)
+    if tipo == "Atropello":
+        predicciones = atropello.predict(df_fechas)
+    elif tipo == "Caída de Ocupante":
+        predicciones = caida.predict(df_fechas)
+    elif tipo == "Choque":
+        predicciones = choque.predict(df_fechas)
+    elif tipo == "Incendio":
+        predicciones = list()
+        for _ in range(df_fechas.shape[0]):
+            r = random.random()
+            if r<=0.012438916037316748:
+                predicciones.append(1)
+            else:
+                predicciones.append(0)
+        predicciones = np.asarray(predicciones)
+    elif tipo == "Volcamiento":
+        predicciones = volcamiento.predict(df_fechas)
+    elif tipo == "Otro":
+        predicciones = otro.predict(df_fechas)
+        predicciones = np.asarray(tuple(map(piso_techo, predicciones)))
 
-st.subheader(f'Entre el {finicial} y el {f2} habrán {int(np.sum(predicciones))} accidentes de tipo "{tipo}"\n')
+    st.subheader(f'Entre el {finicial} y el {f2} habrán {int(np.sum(predicciones))} accidentes de tipo "{tipo}"\n')
 
 # st.subheader("Separando los accidentes por el dia en el que ocurrieron, tenemos los siguientes resultados: \n")
 # sns.set()
@@ -68,5 +68,5 @@ st.subheader(f'Entre el {finicial} y el {f2} habrán {int(np.sum(predicciones))}
 # st.pyplot(fig1)
     
 
-# except:
-#     st.markdown("# Esperando rango de fechas...")
+except:
+    st.markdown("# Esperando rango de fechas...")
